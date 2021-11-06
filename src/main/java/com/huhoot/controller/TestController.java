20a7efa8d14bdfe9e863f70fe6a4c80a7ba9a8b1
huyvu8051 <huyvu8051@gmail.com>
2021-11-07 02:06:17 +0700

    add redirect homepage to swagger-ui.html

--- src/main/java/com/huhoot/controller/TestController.java
+++ src/main/java/com/huhoot/controller/TestController.java
@@ -2,11 +2,12 @@ package com.huhoot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TestController {
-	@GetMapping("/")
-	public String test() {
-		return "Hello my friend :)";
-	}
+    @GetMapping("/")
+    public ModelAndView test() {
+        return new ModelAndView("redirect:/swagger-ui.html");
+    }
 }
