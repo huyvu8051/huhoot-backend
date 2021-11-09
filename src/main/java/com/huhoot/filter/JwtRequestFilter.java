2dac3282c5fdc0a69ac74f54af10a40323b59e8f
huyvu8051 <huyvu8051@gmail.com>
2021-11-10 02:02:38 +0700

    ??

--- src/main/java/com/huhoot/filter/JwtRequestFilter.java
+++ src/main/java/com/huhoot/filter/JwtRequestFilter.java
@@ -1,12 +1,8 @@
 package com.huhoot.filter;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.huhoot.service.impl.MyUserDetailsService;
+import com.huhoot.utils.jwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,50 +11,57 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.huhoot.service.impl.MyUserDetailsService;
-import com.huhoot.utils.jwtUtil;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@Slf4j
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
-	@Autowired
-	private MyUserDetailsService myUserDetailsService;
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
 
-	@Autowired
-	private jwtUtil jwtUtil;
+    @Autowired
+    private jwtUtil jwtUtil;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		final String authorizaionHeader = request.getHeader("Authorization");
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-		String username = null;
-		String jwt = null;
+        log.warn("is exception from here ?");
 
-		if (authorizaionHeader != null && authorizaionHeader.startsWith("Bearer ")) {
-			jwt = authorizaionHeader.substring(7);
-			username = jwtUtil.extractUsername(jwt);
-		}
+        final String authorizaionHeader = request.getHeader("Authorization");
 
-		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
-			if (jwtUtil.validateToken(jwt, userDetails)) {
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-						userDetails, null, userDetails.getAuthorities());
+        String username = null;
+        String jwt = null;
 
-				usernamePasswordAuthenticationToken
-						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        if (authorizaionHeader != null && authorizaionHeader.startsWith("Bearer ")) {
+            jwt = authorizaionHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
 
-				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
 
-			}
-		}
-		filterChain.doFilter(request, response);
-	}
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-	@Override
-	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-		String path = request.getRequestURI();
-		return "/authentication".equals(path) || path.startsWith("/resources");
-	}
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return "/authentication".equals(path) || path.startsWith("/resources");
+    }
 
 }
