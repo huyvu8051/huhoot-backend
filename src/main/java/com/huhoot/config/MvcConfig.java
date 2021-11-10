ab593ddcb353963ff876be93e9000677b389e66a
huyvu8051 <huyvu8051@gmail.com>
2021-11-10 22:06:07 +0700

    try upload file in system path

--- src/main/java/com/huhoot/config/MvcConfig.java
+++ src/main/java/com/huhoot/config/MvcConfig.java
@@ -1,27 +1,21 @@
 package com.huhoot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        exposeDirectory("resources", registry);
-    }
-
-    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-            Path uploadDir = Paths.get(dirName);
-            String uploadPath = uploadDir.toFile().getAbsolutePath();
-
-        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-
-        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/" + uploadPath + "/");
+        registry.addResourceHandler("resources/**").addResourceLocations("file:/resources/");
     }
 }
--- src/main/java/com/huhoot/utils/UploadFileUtils.java
+++ src/main/java/com/huhoot/utils/UploadFileUtils.java
@@ -1,17 +1,17 @@
 package com.huhoot.utils;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
 @Component
@@ -19,19 +19,17 @@ public class UploadFileUtils {
 
     public String uploadImage(String originalFileName, String base64) throws IOException {
 
-        String imagesPath = this.getDirectPath();
+        Path uploadPath = Paths.get("/resources/");
 
-        File folder = new File(imagesPath);
-
-        if (!folder.exists()) {
-            folder.mkdir();
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
         }
 
         String filename = this.generateFileName(originalFileName);
 
         byte[] bytes = Base64.decodeBase64(base64);
 
-        try (FileOutputStream fileOutputStream = new FileOutputStream(new File(imagesPath + "/" + filename))) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(new File(uploadPath + "/" + filename))) {
             fileOutputStream.write(bytes);
         } catch (IOException e) {
 
@@ -42,13 +40,30 @@ public class UploadFileUtils {
         return filename;
     }
 
+    public static void saveFile(String uploadDir, String fileName,
+                                MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
+
     private String generateFileName(String originalFileName) {
         return UUID.randomUUID().toString() + originalFileName.substring(originalFileName.lastIndexOf("."));
     }
 
     private String getDirectPath() throws IOException {
-        Path uploadDir = Paths.get("resources");
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
+        Path uploadDir = Paths.get("images");
+        // String uploadPath = uploadDir.toFile().getAbsolutePath();
+        String uploadPath = new File(".").getCanonicalPath() + "/src/main/resources/images/";
         return uploadPath;
     }
 }
